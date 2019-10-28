f5ed453651e496af176721639f972586ef48dc68
tan doan <tan.doan@inapps.net>
2019-10-28 16:05:00 +0700

    update param

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const roomService = require('_services/rooms.services')
 
 // input
-const arrivals = [1, 3, 5]
-const departures = [2, 6, 10]
+const arrivals = [1, 3, 5, 7, 9, 11, 13]
+const departures = [2, 6, 10, 10, 11, 15]
 const K = 1
 
 // check avaliable
